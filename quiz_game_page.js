c6368f7ad72b6d6ea3628ce67080ae67898a5194
mahikam <76594479+mahikam@users.noreply.github.com>
2021-04-28 13:38:16 -0400

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,27 @@
+player_1_name=localStorage.getItem("Player 1");
+player_2_name=localStorage.getItem("Player 2");
+
+player_1_score=0;
+player_2_score=0;
+
+document.getElementById("player1_name").innerHTML=player_1_name+" : ";
+document.getElementById("player2_name").innerHTML=player_2_name+" : ";
+document.getElementById("player1_score").innerHTML=player_1_score;
+document.getElementById("player2_score").innerHTML=player_2_score;
+
+document.getElementById("player_question").innerHTML="Question - "+player_1_name;
+document.getElementById("player_answer").innerHTML="Answer - "+player_2_name;
+
+function send(){
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    actual_answer=parseInt(number1)*parseInt(number2);
+
+    question_number= "<h4>"+number1+" X "+number2+"</h4>";
+    input_box= "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button= "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row= question_number+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("number1").innerHTML="";
+    document.getElementById("number2").innerHTML="";
+}
